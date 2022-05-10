16a8065d37bf1c4fb972d6d92722da4c2d276f09
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-05-10 13:20:50 +0200

    Fix broken API when project selector has empty input

--- src/app/gitlab-projects/modules/project-selector/project-selector.component.ts
+++ src/app/gitlab-projects/modules/project-selector/project-selector.component.ts
@@ -28,47 +28,51 @@ export class ProjectSelectorComponent implements OnInit {
 
   ngOnInit(): void {
     this.filteredOptions$ = this.txtInput.valueChanges.pipe(
-      debounceTime(1000),
+      debounceTime(200),
       mergeMap(value => {
-        // search for id
-        const id = Number(value);
-        const projectById$ = (isNaN(id) ? of([]) : this.projects.getProjectById(id)
-          .pipe(
-            // transform to array of length 1 to make it joinable
-            map(project => [project]),
-            catchError(() => [])
-          ))
-          .pipe(
-            // transform to filterset
-            map(items => ({items, total: items.length}) as FilterSet<GitlabProject>)
-          );
-        // search for name
-        const projectsByName$ = (value.length < 3 ? of([]) : this.projects.getProjects(value)
-          .pipe(
-            // exclude project with id
-            filter(set => set.payload.id != id),
-            // do not catch all entries,  only 20
-            take(this.optionsLimit),
-            toArray(),
-            catchError(() => [])
-          ))
-          .pipe(
-            map(sets => ({
-              items: sets.map(set => set.payload),
-              total: sets.length > 0 ? sets[0].total : 0
-            } as FilterSet<GitlabProject>))
-          )
-        return forkJoin([projectById$, projectsByName$])
-          .pipe(
-            map(result => ({
-              items: [...result[0].items, ...result[1].items],
-              total: result[0].total + result[1].total
-            } as FilterSet<GitlabProject>)),
-            tap(set => {
-              this.filteredOptionsMissing = set.total - set.items.length;
-            }),
-            map(set => set.items)
-          );
+        if (value === '') {
+          return of([]);
+        } else {
+          // search for id
+          const id = Number(value);
+          const projectById$ = (isNaN(id) ? of([]) : this.projects.getProjectById(id)
+            .pipe(
+              // transform to array of length 1 to make it joinable
+              map(project => [project]),
+              catchError(() => [])
+            ))
+            .pipe(
+              // transform to filterset
+              map(items => ({items, total: items.length}) as FilterSet<GitlabProject>)
+            );
+          // search for name
+          const projectsByName$ = (value.length < 3 ? of([]) : this.projects.getProjects(value)
+            .pipe(
+              // exclude project with id
+              filter(set => set.payload.id != id),
+              // do not catch all entries,  only 20
+              take(this.optionsLimit),
+              toArray(),
+              catchError(() => [])
+            ))
+            .pipe(
+              map(sets => ({
+                items: sets.map(set => set.payload),
+                total: sets.length > 0 ? sets[0].total : 0
+              } as FilterSet<GitlabProject>))
+            )
+          return forkJoin([projectById$, projectsByName$])
+            .pipe(
+              map(result => ({
+                items: [...result[0].items, ...result[1].items],
+                total: result[0].total + result[1].total
+              } as FilterSet<GitlabProject>)),
+              tap(set => {
+                this.filteredOptionsMissing = set.total - set.items.length;
+              }),
+              map(set => set.items)
+            );
+        }
       })
     );
   }
