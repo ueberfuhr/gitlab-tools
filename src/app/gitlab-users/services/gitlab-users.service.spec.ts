1a1a27cb9b8cb9fcf93d333fc4f8718d6d954ca8
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-05-14 05:48:52 +0200

    Add test for HttpErrorResponse

--- src/app/gitlab-users/services/gitlab-users.service.spec.ts
+++ src/app/gitlab-users/services/gitlab-users.service.spec.ts
@@ -1,34 +1,53 @@
 import {GitlabUsersService} from './gitlab-users.service';
-import {createServiceFactory, SpectatorService} from '@ngneat/spectator/jest';
+import {createServiceFactory, SpectatorService, SpyObject} from '@ngneat/spectator/jest';
 import {GitlabService} from '../../gitlab-access/services/gitlab.service';
-import {of} from 'rxjs';
+import {of, throwError} from 'rxjs';
+import {HttpErrorResponse} from '@angular/common/http';
 
 describe('GitlabUsersService', () => {
 
-  let spectator: SpectatorService<GitlabUsersService>;
   const createService = createServiceFactory({
     service: GitlabUsersService,
     mocks: [GitlabService]
   });
 
-  beforeEach(() => spectator = createService());
+  let spectator: SpectatorService<GitlabUsersService>;
+  // Mock
+  let gitlab: SpyObject<GitlabService>;
+  // Class under Test
+  let users: GitlabUsersService;
+
+  beforeEach(() => {
+    spectator = createService();
+    gitlab = spectator.inject(GitlabService);
+    users = spectator.service;
+  });
 
   it('should reduce to GitlabUser', done => {
-    spectator.inject(GitlabService).call
-      .mockReturnValue(of({
-        id: 10,
-        username: 'john.doe',
-        name: 'John Doe',
-        another_property: 22
-      }));
-    spectator.service.getCurrentUser()
-      .subscribe(user => {
-        expect(user.id).toBe(10);
-        expect(user.username).toBe('john.doe');
-        expect(user.name).toBe('John Doe');
-        expect((user as any).another_property).toBeUndefined();
+    gitlab.call.mockReturnValue(of({
+      id: 10,
+      username: 'john.doe',
+      name: 'John Doe',
+      another_property: 22
+    }));
+    users.getCurrentUser().subscribe(user => {
+      expect(user.id).toBe(10);
+      expect(user.username).toBe('john.doe');
+      expect(user.name).toBe('John Doe');
+      expect((user as any).another_property).toBeUndefined();
+      done();
+    });
+  }, 1000);
+
+  it('should not catch any error', done => {
+    const errorResponse = new HttpErrorResponse({});
+    gitlab.call.mockReturnValue(throwError(() => errorResponse));
+    users.getCurrentUser().subscribe({
+      error: err => {
+        expect(err).toBe(errorResponse);
         done();
-      });
+      }
+    });
   }, 1000);
 
 });
