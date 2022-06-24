b3b5270a6ee1fdfa873ab2bad2b60d0eade20ee7
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-06-24 09:08:41 +0200

    Introduce landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [{
-  path: 'issues',
-  loadChildren: () => import('./gitlab-issues/gitlab-issues.module').then(m => m.GitlabIssuesModule)
-}];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule)
+  },
+  {
+    path: 'issues',
+    loadChildren: () => import('./gitlab-issues/gitlab-issues.module').then(m => m.GitlabIssuesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
